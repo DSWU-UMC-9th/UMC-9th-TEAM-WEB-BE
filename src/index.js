05992d03698264d71dcb8caa76edd2f5d7b2b070
JCTA0125 <jcta0125@duksung.ac.kr>
2025-11-19 17:54:24 +0900

    feat: 프로젝트 초기 세팅
    - 기본 구성 파일 생성
    - 주요 라이브러리 설치

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import express from 'express'; 
+import cors from "cors";
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import swaggerAutogen from "swagger-autogen";
+import swaggerUiExpress from "swagger-ui-express";
+
+const app = express()
+const port = process.env.PORT;
+
+app.use(morgan('dev'));
+app.use(cookieParser()); 
+app.use(cors()); 
+app.use(express.static('public'));
+app.use(express.json());                 
+app.use(express.urlencoded({ extended: false }));
+
+
+
+app.use(
+  "/docs",
+  swaggerUiExpress.serve,
+  swaggerUiExpress.setup({}, {
+    swaggerOptions: {
+      url: "/openapi.json",
+    },
+  })
+);
+
+app.get("/openapi.json", async (req, res, next) => {
+  // #swagger.ignore = true
+  const options = {
+    openapi: "3.0.0",
+    disableLogs: true,
+    writeOutputFile: false,
+  };
+  const outputFile = "/dev/null"; // 파일 출력은 사용하지 않습니다.
+  const routes = ["./src/index.js"];
+  const doc = {
+    info: {
+      title: "PAGE PARTNER",
+      description: "UMC 9th Mini Project WEB TEAM",
+    },
+    host: "localhost:3000",
+  };
+
+  const result = await swaggerAutogen(options)(outputFile, routes, doc);
+  res.json(result ? result.data : null);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
