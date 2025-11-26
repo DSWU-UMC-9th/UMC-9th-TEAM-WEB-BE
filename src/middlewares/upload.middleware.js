896d7a9092e25a12cd681182627546193e9c892a
HaSam <ssam8610@naver.com>
2025-11-26 11:52:06 +0900

    feat: s3 이미지 파일명 uniqueSuffix 추가

--- src/middlewares/upload.middleware.js
+++ src/middlewares/upload.middleware.js
@@ -1,6 +1,7 @@
 import { S3Client } from "@aws-sdk/client-s3";
 import multer from "multer";
 import multerS3 from 'multer-s3'
+import path from 'path';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -19,7 +20,13 @@ const upload = multer({
         bucket: process.env.AWS_S3_BUCKET,
         contentType: multerS3.AUTO_CONTENT_TYPE,
         key: (req, file, cb) => {
-            cb(null, file.originalname) // 파일명을 그대로 사용
+            const originalFileName = file.originalname; // 원본 파일명
+            const ext = path.extname(originalFileName); // 파일 확장자 추출
+            const basename = path.basename(originalFileName, ext); // 원본 파일명에서 확장자를 제외한 부분
+            const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9); // 고유한 접미사
+            const fileName = `${basename}-${uniqueSuffix}${ext}`; // 원본 파일명 + 접미사 + 확장자            
+            cb(null, `books/${fileName}`) // 파일명을 그대로 사용
+            
         }
     })
 })
